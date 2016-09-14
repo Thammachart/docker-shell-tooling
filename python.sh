64819db4dd5dd4e9f6c0ef9f76fb4cdce101875c
Thammachart <bustheheroall@gmail.com>
2016-09-14 17:26:01 +0700

    Just thought of simpler solution, just prepend 'python' command :)

--- python.sh
+++ python.sh
@@ -1,7 +1,7 @@
 python2() {
-    docker-cmd -it --entrypoint=python python:2-alpine $@
+    docker-cmd -it python:2-alpine python $@
 }
 
 python3() {
-    docker-cmd -it --entrypoint=python python:3-alpine $@
+    docker-cmd -it python:3-alpine python $@
 }
