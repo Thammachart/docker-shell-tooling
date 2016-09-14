76b19d2e658321634028fec74a1b3ee99d40036e
Thammachart <bustheheroall@gmail.com>
2016-09-14 17:23:41 +0700

    Default entrypoint of Python Alpine Docker is changed to 'python'

--- python.sh
+++ python.sh
@@ -1,7 +1,7 @@
 python2() {
-    docker-cmd -it python:2-alpine $@
+    docker-cmd -it --entrypoint=python python:2-alpine $@
 }
 
 python3() {
-    docker-cmd -it python:3-alpine $@
+    docker-cmd -it --entrypoint=python python:3-alpine $@
 }
