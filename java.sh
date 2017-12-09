d5e7950189d5a2d26bf47c61e3088746becc50d8
Thammachart <bustheheroall@gmail.com>
2017-12-09 16:26:17 +0700

    java8 command added

--- java.sh
+++ java.sh
@@ -2,10 +2,22 @@ java-cmd() {
     docker-cmd openjdk:9-jdk-slim $@
 }
 
+java8-cmd() {
+    docker-cmd openjdk:8-jdk-slim $@
+}
+
 java() {
     java-cmd java $@
 }
 
 javac() {
     java-cmd javac $@
+}
+
+java8() {
+    java8-cmd java $@
+}
+
+java8c() {
+    java8-cmd javac $@
 }
